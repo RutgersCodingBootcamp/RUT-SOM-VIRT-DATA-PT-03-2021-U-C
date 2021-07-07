a7a20a3fb7de5c83f6df0f88adc64720d95af1b8
gretelup <gretelup@gmail.com>
2021-07-07 15:55:13 -0400

    Add 14.2 Activities

---
+++ 01-Lesson-Plans/14-Interactive-Web-Visualizations/2/Activities/01-Ins_Mapping/Unsolved/forEach.js
@@ -0,0 +1,8 @@
+let theStagesOfJS = ["confidence", "sadness", "confusion", "realization", "debugging", "satisfaction"];
+
+console.log(theStagesOfJS)
+
+// Using the old school iterative way
+
+
+// Using the forEach method
---
+++ 01-Lesson-Plans/14-Interactive-Web-Visualizations/2/Activities/01-Ins_Mapping/Unsolved/map.js
@@ -0,0 +1,4 @@
+let theStagesOfJS = ["confidence", "sadness", "confusion", "realization", "debugging", "satisfaction"];
+
+console.log(theStagesOfJS)
+
