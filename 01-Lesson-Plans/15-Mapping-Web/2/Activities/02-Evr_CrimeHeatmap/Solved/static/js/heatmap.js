4d9c6bed566af092464a5cb082ced8f5f7f01869
Jen-Dean <missjenniferdean@gmail.com>
2021-07-17 09:17:24 -0400

    Switching Solved and Unsolved Folders

--- 01-Lesson-Plans/15-Mapping-Web/2/Activities/02-Evr_CrimeHeatmap/Solved/static/js/heatmap.js
+++ 01-Lesson-Plans/15-Mapping-Web/2/Activities/02-Evr_CrimeHeatmap/Solved/static/js/heatmap.js
@@ -1,17 +1,32 @@
-// Create map
+var myMap = L.map("map", {
+  center: [37.7749, -122.4194],
+  zoom: 13
+});
 
 // Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
-// link to get crime data
 var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
 
-// Read json
+d3.json(url).then(function(response) {
 
+  console.log(response);
 
-  // console.log(response);
-
-// create empty heat array for coordinates
   var heatArray = [];
-// Loop through responses and only add those with locations to the array
 
-// Add Heat layer to map
+  for (var i = 0; i < response.length; i++) {
+    var location = response[i].location;
+
+    if (location) {
+      heatArray.push([location.coordinates[1], location.coordinates[0]]);
+    }
+  }
+
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(myMap);
+
+});
--- 01-Lesson-Plans/15-Mapping-Web/2/Activities/02-Evr_CrimeHeatmap/Unsolved/static/js/heatmap.js
+++ 01-Lesson-Plans/15-Mapping-Web/2/Activities/02-Evr_CrimeHeatmap/Unsolved/static/js/heatmap.js
@@ -1,32 +1,17 @@
-var myMap = L.map("map", {
-  center: [37.7749, -122.4194],
-  zoom: 13
-});
+// Create map
 
 // Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
 
+// link to get crime data
 var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
 
-d3.json(url).then(function(response) {
+// Read json
 
-  console.log(response);
 
+  // console.log(response);
+
+// create empty heat array for coordinates
   var heatArray = [];
+// Loop through responses and only add those with locations to the array
 
-  for (var i = 0; i < response.length; i++) {
-    var location = response[i].location;
-
-    if (location) {
-      heatArray.push([location.coordinates[1], location.coordinates[0]]);
-    }
-  }
-
-  var heat = L.heatLayer(heatArray, {
-    radius: 20,
-    blur: 35
-  }).addTo(myMap);
-
-});
+// Add Heat layer to map
